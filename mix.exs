b04f3a86d0815ece95cd126b3e4280a8c59f8b58
Daniel Ribeiro Moreira <daniel@ribeiromoreira.com>
2019-12-16 15:38:44 -0300

    adds ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -7,22 +7,21 @@ defmodule TruffleHog.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
@@ -35,9 +34,8 @@ defmodule TruffleHog.MixProject do
   defp package do
     [
       name: "truffle_hog",
-      organization: "quero",
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      maintainers: ["pufe"],
+      files: ~w(lib/**/*.ex test/**/*.exs .formatter.exs mix.exs README.md),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/pufe/truffle_hog"},
       source_url: "https://github.com/pufe/truffle_hog"
